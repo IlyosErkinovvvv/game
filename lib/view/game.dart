8ea398688410657f4d43d2cd7da0c4d4615bd0f0
salohiddin-65 <salohiddink65@gmail.com>
2023-05-26 15:03:25 +0500

    first commit

---
+++ lib/view/game.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  int red = 5;
+  int blue = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: red.toInt(),
+              child: InkWell(
+                onTap: () {
+                  setState(
+                    () {
+                      if (red > 9) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text(
+                                'Blue has won!\nDo you want to play new game?'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  exit(0);
+                                },
+                                child: const Text('No'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  setState(() {
+                                    red = 5;
+                                    blue = 5;
+                                  });
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      red += 1;
+                      blue -= 1;
+                    },
+                  );
+                },
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: blue.toInt(),
+              child: InkWell(
+                onTap: () {
+                  setState(
+                    () {
+                      if (blue > 9) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text(
+                                'Red has won!\nDo you want to play new game?'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  exit(0);
+                                },
+                                child: const Text('No'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  setState(() {
+                                    red = 5;
+                                    blue = 5;
+                                  });
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      red = 5;
+                      blue = 5;
+                    },
+                  );
+                },
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
